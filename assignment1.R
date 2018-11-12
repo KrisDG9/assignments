80c3aa2dc54fa0c851c4368d87b2dad68bd3757d
KrisDG9 <mw.k.degroot@gmail.com>
2018-11-12 15:44:47 +0100

    First commit - data.

---
+++ assignment1.R
@@ -0,0 +1,12 @@
+#assignment1
+
+#Old forbes data: ranking of the richest people on earth.
+
+data = read.csv("forbes.csv", header = TRUE)
+
+install.packages("lintr")
+
+library(lintr)
+
+# q1 ----------------------------------------------------------------------
+
